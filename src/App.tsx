f7e07bc58e3f574cd5cf29d6803550570a5d436b
Rocio Alcala <rro.alcala@gmail.com>
2023-12-14 13:19:11 -0300

    chore: rendering Inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import Input from "./Input";
+import fields from "../fields.json"
 
 function App() {
-  return <></>;
+  return (
+    <form className="container">
+      {fields.map((field, index) => {
+        if (Array.isArray(field)) {
+          return (
+            <div className="rowcontainer">
+              {field.map((columField, index) => {
+                return <Input field={columField} key={index}></Input>;
+              })}
+            </div>
+          );
+        } else {
+          return <Input field={field} key={index}></Input>;
+        }
+      })}
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default App;
